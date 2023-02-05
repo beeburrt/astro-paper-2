4b645e8601405880a7a31b0b2a1fea8c26b7b386
Brian Burt <beeburrt@outlook.com>
2023-02-04 19:38:06 -0800

    change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/",
+  site: "https://astro-paper2.pages.dev/",
   integrations: [
     tailwind({
       config: {
